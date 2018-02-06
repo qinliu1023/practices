b261dadf01d9ccc499d5cf5bb70bbd7d2adf8e32
qinliu1023 <31944640+qinliu1023@users.noreply.github.com>
2018-02-05 19:44:44 -0500

    Update 136. Single Number.py

--- 136. Single Number.py
+++ 136. Single Number.py
@@ -36,3 +36,33 @@ class Solution(object):
         :rtype: int
         """
         return 2 * sum(set(nums)) - sum(nums)
+
+
+# A similar question can be solved using Math    
+"""
+Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, find the one that is missing from the array.
+
+Example 1
+
+Input: [3,0,1]
+Output: 2
+Example 2
+
+Input: [9,6,4,2,3,5,7,0,1]
+Output: 8
+
+Note:
+Your algorithm should run in linear runtime complexity. Could you implement it using only constant extra space complexity?    
+"""
+    
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # number starts from 0 to n, and missing one of them
+        # if number not missing the sum should be sum(range(n))
+        # the difference between sum(range(n)) and sum(nums) is the number missing
+        n = len(nums) + 1
+        return sum(range(n)) - sum(nums)
