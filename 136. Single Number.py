39691cb575f6474cd2af05a0941c6e456a8769fe
qinliu1023 <31944640+qinliu1023@users.noreply.github.com>
2018-02-06 23:59:46 -0500

    Update 136. Single Number.py

--- 136. Single Number.py
+++ 136. Single Number.py
@@ -69,6 +69,14 @@ class Solution(object):
         return sum(range(n)) - sum(nums)
 
 # 137. Single Number II
+"""
+Given an array of integers, every element appears three times except for one, which appears exactly once. Find that single one.
+
+Note:
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+"""
+
+
 
 # 260. Single Number III
 """
