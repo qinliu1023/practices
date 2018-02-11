1b012e86fecff8c817b3b5ce006904b984023635
qinliu1023 <31944640+qinliu1023@users.noreply.github.com>
2018-02-10 23:15:29 -0500

    Update 120. Triangle.py

--- 120. Triangle.py
+++ 120. Triangle.py
@@ -21,3 +21,40 @@ where n is the total number of rows in the triangle.
 # Similar to Binary Tree
 # Traverse: Try all possible path, Time Complexity O(2^n)
 # DivideConquer: Time Complexity O(2^n)
+
+
+# 64. Minimum Path Sum
+"""
+Given a m x n grid filled with non-negative numbers, 
+find a path from top left to bottom right which minimizes the sum of all numbers along its path.
+
+Note: You can only move either down or right at any point in time.
+
+Example 1:
+[[1,3,1],
+ [1,5,1],
+ [4,2,1]]
+
+Given the above grid map, return 7. Because the path 1→3→1→1→1 minimizes the sum.
+"""
+
+
+# 62. Unique Paths
+"""
+A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+
+The robot can only move either down or right at any point in time. 
+The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+
+How many possible unique paths are there?
+
+Above is a 3 x 7 grid. How many possible unique paths are there?
+
+Note: m and n will be at most 100.
+"""
+# Math combination: C
+"""
+[1,1,1,1,1]
+[1,2,3,4,5]
+[1,3,6,10,1]
+"""
