d97dd5d9abd8d0bd730bf16d14663182d097b668
qinliu1023 <31944640+qinliu1023@users.noreply.github.com>
2018-02-13 08:24:41 -0500

    Add files via upload

---
+++ 181. Employees Earning More Than Their Managers.sql
@@ -0,0 +1,58 @@
+/*
+SQL Schema
+
+Create table If Not Exists Employee (Id int, Name varchar(255), Salary int, ManagerId int)
+Truncate table Employee
+insert into Employee (Id, Name, Salary, ManagerId) values ('1', 'Joe', '70000', '3')
+insert into Employee (Id, Name, Salary, ManagerId) values ('2', 'Henry', '80000', '4')
+insert into Employee (Id, Name, Salary, ManagerId) values ('3', 'Sam', '60000', 'None')
+insert into Employee (Id, Name, Salary, ManagerId) values ('4', 'Max', '90000', 'None')
+
+
+
+The Employee table holds all employees including their managers. Every employee has an Id, and there is also a column for the manager Id.
+
++----+-------+--------+-----------+
+| Id | Name  | Salary | ManagerId |
++----+-------+--------+-----------+
+| 1  | Joe   | 70000  | 3         |
+| 2  | Henry | 80000  | 4         |
+| 3  | Sam   | 60000  | NULL      |
+| 4  | Max   | 90000  | NULL      |
++----+-------+--------+-----------+
+Given the Employee table, write a SQL query that finds out employees who earn more than their managers. For the above table, Joe is the only employee who earns more than his manager.
+
++----------+
+| Employee |
++----------+
+| Joe      |
++----------+
+*/
+# Write your MySQL query statement below
+SELECT Employee
+FROM (
+    SELECT e1.Name AS Employee, e2.Name AS Manager, e1.Salary AS Employee_Salary, e2.Salary AS Manager_Salary
+    FROM Employee AS e1
+    LEFT JOIN Employee AS e2
+    ON e1.ManagerId = e2.Id) AS temp_table
+WHERE Manager_Salary < Employee_Salary;
+
+
+# Another Solutions
+SELECT
+     a.NAME AS Employee
+FROM Employee AS a JOIN Employee AS b
+     ON a.ManagerId = b.Id
+     AND a.Salary > b.Salary
+;
+
+
+SELECT
+    *
+FROM
+    Employee AS a,
+    Employee AS b
+WHERE
+    a.ManagerId = b.Id
+        AND a.Salary > b.Salary
+;
